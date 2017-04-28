bcb8f344de0bb858b13aee965507f9c636d4404e
renatog <renatog@3326031.no-reply.drupal.org>
2017-04-28 11:59:12 -0300

    Issue #2873713 by RenatoG: [D8] Create message with success on Send Invite

--- modules/invite_by_email/src/Plugin/Invite/InviteByEmail.php
+++ modules/invite_by_email/src/Plugin/Invite/InviteByEmail.php
@@ -4,6 +4,7 @@ namespace Drupal\invite_by_email\Plugin\Invite;
 
 use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\invite\InvitePluginInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Class for Invite by Email.
@@ -14,6 +15,8 @@ use Drupal\invite\InvitePluginInterface;
  * )
  */
 class InviteByEmail implements InvitePluginInterface {
+
+  use StringTranslationTrait;
 
   /**
    * {@inheritdoc}
@@ -28,7 +31,7 @@ class InviteByEmail implements InvitePluginInterface {
     $mail = \Drupal::service('plugin.manager.mail');
     $mail_key = $invite->get('type')->value;
     // Prepare message.
-    $message = $mail->mail('invite_by_email', $mail_key, $invite->get('field_invite_email_address')->value, $invite->activeLangcode, array(), $invite->getOwner()
+    $message = $mail->mail('invite_by_email', $mail_key, $invite->get('field_invite_email_address')->value, $invite->activeLangcode, [], $invite->getOwner()
       ->getEmail(), FALSE);
     // If HTML email.
     if (unserialize(\Drupal::config('invite.invite_type.' . $invite->get('type')->value)
@@ -36,19 +39,29 @@ class InviteByEmail implements InvitePluginInterface {
     ) {
       $message['headers']['Content-Type'] = 'text/html; charset=UTF-8;';
     }
-    $message['subject'] = $token->replace($invite->get('field_invite_email_subject')->value, array('invite' => $invite), array(), $bubbleable_metadata);
-    $body = array(
+    $message['subject'] = $token->replace($invite->get('field_invite_email_subject')->value, ['invite' => $invite], [], $bubbleable_metadata);
+    $body = [
       '#theme' => 'invite_by_email',
-      '#body' => $token->replace($invite->get('field_invite_email_body')->value, array('invite' => $invite), array(), $bubbleable_metadata),
-    );
+      '#body' => $token->replace($invite->get('field_invite_email_body')->value, ['invite' => $invite], [], $bubbleable_metadata),
+    ];
     $message['body'] = \Drupal::service('renderer')
       ->render($body)
       ->__toString();
     // Send.
-    $system = $mail->getInstance(array(
+    $system = $mail->getInstance([
       'module' => 'invite_by_email',
       'key' => $mail_key,
-    ));
+    ]);
+
+    $result = $system->mail($message);
+
+    if ($result) {
+      drupal_set_message($this->t('Invitation has been sent.'));
+    }
+    else {
+      drupal_set_message($this->t('Failed to send a message.'), 'error');
+    }
+
     $system->mail($message);
   }
 
